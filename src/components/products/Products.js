1e78979c6e7680f95e26684336bcb58a330a996f
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-25 15:10:23 +0430

    qCounter-function / isInCart-function / cart-state and dispatch used in the products-component

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,16 +1,38 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { useDispatch , useSelector } from 'react-redux';
+import { addItem, increase } from '../../redux/cart/cartActions';
 
 //functions
-import { shortTheTitle } from '../../services/functions';
+import { isInCart, qCounter, shortTheTitle } from '../../services/functions';
+
+//tools
+import trashCan from '../../tools/trash-can.png';
 
 const Products = (props) => {
     const {id , title , image , price} = props.productData;
+    const dispatch = useDispatch();
+    const state = useSelector(state => state.cartState);
+
     return (
         <div>
             <img src={image} alt="product" />
             <h1>{shortTheTitle(title)}</h1>
             <h4>{price} $</h4>
-            <button>Add to cart</button>
+            <div>
+                <Link to={`/productdetail/${id}`}>Details</Link>
+                {
+                    qCounter(state , props.productData.id) > 1 && <button onClick={() => dispatch({type: 'DECREASE' , payload: props.productData})}>-</button>
+                }
+                {
+                    qCounter(state , props.productData.id) === 1 && <button onClick={() => dispatch({type: 'REMOVE_ITEM' , payload: props.productData})}><img src={trashCan} alt='remove' /></button>
+                }
+                {
+                    isInCart(state , props.productData.id) ?
+                    <button onClick={() => dispatch(increase(props.productData))}>+</button> :
+                    <button onClick={() => dispatch(addItem(props.productData))}>Add to Cart</button>
+                }
+            </div>
         </div>
     );
 };
--- src/services/functions.js
+++ src/services/functions.js
@@ -3,4 +3,18 @@ const shortTheTitle = (title) => {
     return `${splitedTitle[0]}${splitedTitle[1]}`
 }
 
-export {shortTheTitle};
+const isInCart = (state , id) => {
+    const result = !!state.selectedItems.find(item => item.id === id)
+    return result;
+}
+
+const qCounter = (state , id) => {
+    const index = state.selectedItems.findIndex(item => item.id === id);
+    if (index >=0) {
+        return state.selectedItems[index].quantity;
+    }else {
+        return false
+    }
+}
+
+export {shortTheTitle , isInCart , qCounter};
