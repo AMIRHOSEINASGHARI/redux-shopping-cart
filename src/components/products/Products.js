c010161d66c5af45852d1c44fc231cc444f847a4
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-25 14:53:47 +0430

    short-the-title function

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+//functions
+import { shortTheTitle } from '../../services/functions';
+
 const Products = (props) => {
     const {id , title , image , price} = props.productData;
     return (
         <div>
             <img src={image} alt="product" />
-            <h1>{title}</h1>
+            <h1>{shortTheTitle(title)}</h1>
             <h4>{price} $</h4>
             <button>Add to cart</button>
         </div>
