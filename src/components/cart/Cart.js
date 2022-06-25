186e8cbd7009d65a88d54677ba752cf8fb6062a6
Amirhosein Asghari <amirhoseinasghari2000@gmail.com>
2022-06-25 16:24:28 +0430

    cart-component styles / cartProduct-component styles

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,16 +4,19 @@ import {Link} from 'react-router-dom';
 import CartProduct from './CartProduct';
 import { useDispatch , useSelector } from 'react-redux';
 
+//styles
+import styles from './Cart.module.css';
+
 const Cart = () => {
     const dispatch = useDispatch();
     const cartState = useSelector(state => state.cartState)
 
     return (
-        <div>
-            <div>
+        <div className={styles.cartContainer}>
+            <div className={styles.topSection}>
                 {
                     cartState.itemsCounter >0 &&
-                    <div>
+                    <div className={styles.cartPayments}>
                         <div>
                             <p><span>Total items:</span> {cartState.itemsCounter}</p>
                             <p><span>Total payments:</span> {cartState.totalPrice} $</p>
@@ -41,7 +44,7 @@ const Cart = () => {
                     </div>
                 }
             </div>
-            <div>
+            <div className={styles.shopCartProduct}>
                 {
                     cartState.selectedItems.map(item => <CartProduct key={item.id} data={item} />)
                 }
--- src/components/cart/CartProduct.js
+++ src/components/cart/CartProduct.js
@@ -8,24 +8,27 @@ import {shortTheTitle} from '../../services/functions';
 //tools
 import trashCan from '../../tools/trash-can.png';
 
+//styles
+import styles from './CartProduct.module.css';
+
 const CartProduct = (props) => {
     const {title , image , price , quantity} = props.data;
     const dispatch = useDispatch();
     return (
-        <div>
-            <img src={image} alt="product" />
-            <div>
+        <div className={styles.shopCartContainer}>
+            <img src={image} alt="product" className={styles.shopCartImage} />
+            <div className={styles.shopCartTitle}>
                 <h1>{shortTheTitle(title)}</h1>
                 <p>{price} $</p>
             </div>
-            <div>
-                <span>{quantity}</span>
+            <div className={styles.productQuantity}>
+                <span className={styles.qCounter}>{quantity}</span>
             </div>
-            <div>
+            <div className={styles.buttonsSectios}>
                 {
                     quantity >1 ?
                     <button onClick={() => dispatch(decrease(props.data))}>-</button> :
-                    <button onClick={() => dispatch(removeItem(props.data))}><img src={trashCan} alt='remove' /></button>
+                    <button onClick={() => dispatch(removeItem(props.data))} className={styles.shopCartTrashCan}><img src={trashCan} alt='remove' /></button>
                 }
                 <button onClick={() => dispatch({type: 'INCREASE' , payload: props.data})}>+</button>
             </div>
